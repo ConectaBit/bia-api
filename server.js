915192e2353972bdf489cb07898be822f1b3ef75
jrquadros <quadrosjunioradits@gmail.com>
2019-06-02 19:31:59 -0300

    api boilerplate

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const porta = 8080;
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api", require("./src/routes"));
+
+app.listen(porta);
+
+console.log("Server rodando na porta " + porta + " XD");
